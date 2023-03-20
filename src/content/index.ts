43d4b010f850dc3e1656b78015d74641af3c753d
Hannes Linder <hannes.linder@bitmovin.com>
2023-03-20 10:38:29 +0100

    Store and ddownload chunks

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,6 +2,8 @@ import { Message, MessageSender, MessageType } from "../interface";
 
 chrome.runtime.onMessage.addListener(messageListener);
 
+const recordedChunks: BlobPart[] = [];
+
 async function messageListener(request: Message, _sender: MessageSender, sendResponse: (response: Message) => void) {
   console.log("Content Script received message: ", request);
 
@@ -17,16 +19,40 @@ async function messageListener(request: Message, _sender: MessageSender, sendRes
 }
 
 async function startCapture(displayMediaOptions: DisplayMediaStreamOptions) {
-  let captureStream = null;
+  let captureStream: MediaStream;
 
-  try {
-    captureStream = await navigator.mediaDevices.getDisplayMedia(
-      displayMediaOptions
-    );
-  } catch (err) {
-    console.error(`Error: ${err}`);
-  }
+  captureStream = await navigator.mediaDevices.getDisplayMedia(
+    displayMediaOptions
+  );
 
   console.log(captureStream);
+  const mediaRecorder = new MediaRecorder(captureStream)
+
+  mediaRecorder.ondataavailable = (event) => handleDataAvailable(event);
   return captureStream;
+}
+
+function handleDataAvailable(event: BlobEvent) {
+  console.log("data-available");
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
+    console.log(recordedChunks);
+    download();
+  } else {
+    
+  }
+}
+
+function download() {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.setAttribute("style", "display: none");
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
+  window.URL.revokeObjectURL(url);
 }
