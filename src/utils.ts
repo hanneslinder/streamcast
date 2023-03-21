6ee8fb2879e078f3324d49e37656080abcf39295
Eric Klatzer <eric@klatzer.at>
2023-03-21 09:04:41 +0100

    adjusted state handling

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,6 @@ export function setState(key: keyof ExtensionState, value: any): Promise<void> {
   return chrome.storage.local.set({[key]: value});
 }
 
-export function getState(key: keyof ExtensionState): Promise<{[key: string]: any}> {
-  return chrome.storage.local.get([key]);
+export function getState(keys: keyof ExtensionState | Array<keyof ExtensionState>): Promise<{[key: string]: any}> {
+  return chrome.storage.local.get(keys);
 }
