0d8f586d81865c3cf91f9c6d52b05a2a0f9e0641
Eric Klatzer <eric@klatzer.at>
2023-03-21 09:51:20 +0100

    WIP: fixed storage stuff

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,13 @@
 import { ExtensionState } from "./interface";
 
-export function setState(key: keyof ExtensionState, value: any): Promise<void> {
-  return chrome.storage.local.set({[key]: value});
+export function setState(key: keyof ExtensionState, value: any, callback : () => void = () => {}) {
+  chrome.storage.session.set({[key]: value}, callback);
+}
+
+export function setStates(items: { [key: string]: any }, callback : () => void = () => {}) {
+  chrome.storage.session.set(items, callback);
 }
 
 export function getState(keys: keyof ExtensionState | Array<keyof ExtensionState>): Promise<{[key: string]: any}> {
-  return chrome.storage.local.get(keys);
+  return chrome.storage.session.get(keys);
 }
